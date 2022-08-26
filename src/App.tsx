7e95504d9d43ef53f289084bdcaac4ffad3cf640
Gwyn Dinh <dinhthanhtai.xz@gmail.com>
2022-08-26 07:41:29 +0700

    refacertor header

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,10 @@
-import { useState } from 'react';
-import 'boxicons';
-
-const navList = [
-  {
-    title: 'Home',
-    href: 'home',
-    icon: 'bx bx-home'
-  },
-  {
-    title: 'Profile',
-    href: 'profile',
-    icon: 'bx bx-user'
-  },
-  {
-    title: 'Education',
-    href: 'education',
-    icon: 'bx bx-book'
-  },
-  {
-    title: 'Skills',
-    href: 'skills',
-    icon: 'bx bx-receipt'
-  },
-  {
-    title: 'Experience',
-    href: 'experience',
-    icon: 'bx bx-briefcase-alt'
-  },
-  {
-    title: 'Certificates',
-    href: 'certificate',
-    icon: 'bx bx-award'
-  },
-  {
-    title: 'References',
-    href: 'references',
-    icon: 'bx bx-link-external'
-  },
-]
+import Header from "./components/header"
 
 function App() {
-  const [toggle, setToggle] = useState(false);
-  const [active, setActive] = useState(0);
 
-  const onClickItem = (index: number) => {
-    setToggle(false);
-    setActive(index)
-  }
 
   return (
-    <header className="l-header" id='header'>
-      <nav className="nav bd-container">
-        <a href="#" className="nav__logo">Gwyn</a>
-
-        <div className={`nav__menu ${toggle && 'show-menu'}`} id="nav-menu">
-          <ul className="nav__list">
-        {
-          navList.map(({title, href, icon}, index) => (
-              <li onClick={() => onClickItem(index)} className="nav__item">
-                <a href={`#${href}`} className={`nav__link ${index === active && 'active-link'}`}>
-                  <i className={icon}></i>{title}
-                </a>
-              </li>
-          ))
-        }
-        </ul>
-        </div>
-     
-
-        <div onClick={() => setToggle(!toggle)} className="nav__toggle" id="nav-toggle">
-          <i className='bx bx-grid-alt'></i>
-        </div>
-      </nav>
-    </header>
+    <Header/>
   )
 }
 
---
+++ src/components/header/index.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+
+const navList = [
+    {
+      title: 'Home',
+      href: 'home',
+      icon: 'bx bx-home'
+    },
+    {
+      title: 'Profile',
+      href: 'profile',
+      icon: 'bx bx-user'
+    },
+    {
+      title: 'Education',
+      href: 'education',
+      icon: 'bx bx-book'
+    },
+    {
+      title: 'Skills',
+      href: 'skills',
+      icon: 'bx bx-receipt'
+    },
+    {
+      title: 'Experience',
+      href: 'experience',
+      icon: 'bx bx-briefcase-alt'
+    },
+    {
+      title: 'Certificates',
+      href: 'certificate',
+      icon: 'bx bx-award'
+    },
+    {
+      title: 'References',
+      href: 'references',
+      icon: 'bx bx-link-external'
+    },
+  ]
+
+const Header: React.FC = () => {
+    const [toggle, setToggle] = useState(false);
+    const [active, setActive] = useState(0);
+  
+    const onClickItem = (index: number) => {
+      setToggle(false);
+      setActive(index)
+    }
+
+    return (
+        <header className="l-header w-full fixed bottom-0 left-0 z-50" id='header'>
+        <nav className="nav bd-container h-12 flex justify-between items-center">
+          <a href="#" className="nav__logo font-medium">Gwyn</a>
+  
+          <div className={`nav__menu ${toggle && 'show-menu'}`} id="nav-menu">
+            <ul className="nav__list grid grid-cols-3 gap-8">
+          {
+            navList.map(({title, href, icon}, index) => (
+                <li key={index} onClick={() => onClickItem(index)} className="nav__item text-center">
+                  <a href={`#${href}`} className={`nav__link flex flex-col ${index === active && 'active-link'}`}>
+                    <i className={`${icon} text-xl mb-2`}></i>{title}
+                  </a>
+                </li>
+            ))
+          }
+          </ul>
+          </div>
+       
+  
+          <div onClick={() => setToggle(!toggle)} className="nav__toggle cursor-pointer text-xl" id="nav-toggle">
+            <i className='bx bx-grid-alt'></i>
+          </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Header;
