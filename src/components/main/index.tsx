caed323f2a14764390290af16bbbc9e9c15c90eb
Gwyn Dinh <dinhthanhtai.xz@gmail.com>
2022-08-30 00:52:41 +0700

    third commit

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -111,10 +111,127 @@ const Main: React.FC = () => {
 
                     <section className='skills section' id='skills'>
                         <h2 className='section-title' > Skill </h2>
+
+                        <div className='skills__content bd-grid'>
+                            <ul className='skills__data'>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> HTML
+                                </li>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> Css
+                                </li>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> Typescript
+                                </li>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> Javascript
+                                </li>
+                            </ul>
+
+                            <ul className='skills__data'>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> React JS
+                                </li>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> Css
+                                </li>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> Typescript
+                                </li>
+                                <li className='skills__name'>
+                                    <span className='skills__circle'></span> Javascript
+                                </li>
+                            </ul>
+                        </div>
                     </section>
                 </div>
-                <div className="resume__right">
 
+                <div className="resume__right">
+                    <section className='experience section' id='experience'>
+                        <h2 className='section-title'> Experience </h2>
+
+                        <div className='experience__container bd-grid'>
+                            <div className='experience__content'>
+                                <div className='experience__time'>
+                                    <span className='experience__rounder'></span>
+                                    <span className='experience__line'></span>
+                                </div>
+
+                                <div className='experience__data bd-grid'>
+                                    <h3 className='experience__title'> Master of design</h3>
+                                    <span className='experience__company'>From 2013 to 2015</span>
+                                    <p className='experience__description'> Work in this company </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className='certificate section' id='certificates'>
+                        <h2 className='section-title'> Certificates </h2>
+
+                        <div className='certificate__container bd-grid'>
+                            <div className='certificate__content'>
+                                <h3 className='certificate__title'> Certified for compliance </h3>
+                                <p className='certificate__description'> For meeting  </p>
+                            </div>
+                        </div>
+                    </section> 
+
+                    <section className='references section' id='references'>
+                        <h2 className='section__tile'> References </h2>
+                        
+                        <div className='references__container bd-grid'>
+                            <div className='references__content bd-grid'>
+                                <span className='references__subtitle'> Sr. Director </span>
+                                <h3 className='references__title'> Mr. Clay Doe </h3>
+                                <ul className='references__contact'>
+                                    <li> Phone: 999-777-666 </li>
+                                    <li> Email: taidinh@kms-technology.com </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className='languages section'>
+                        <h2 className='section-title'> Languages </h2>
+                        
+                        <div className='languages__container'>
+                            <ul className='languages__content bd-grid'>
+                                <li className='languages__name'>
+                                    <span className='languages__circle'></span> Vietnamese
+                                </li>
+                                <li className='languages__name'>
+                                    <span className='languages__circle'></span> English
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+
+                    <section className='interest section'>
+                        <h2 className='section-title'> Interest </h2>
+                    
+                        <div className='interests__container bd-grid'>
+                            <div className='interests__content'>
+                                <i className='bx bx-headphone interests__icon'></i>
+                                <span className='interests__name'> Music </span>
+                            </div>
+
+                            <div className='interests__content'>
+                                <i className='bx bxs-plane-alt interests__icon'></i>
+                                <span className='interests__name'> Travel </span>
+                            </div>
+
+                            <div className='interests__content'>
+                                <i className='bx bx-book interests__icon'></i>
+                                <span className='interests__name'> Read </span>
+                            </div>
+
+                            <div className='interests__content '>
+                                <i className='bx bx-dumbbell interests__icon'></i>
+                                <span className='interests__name'> Fitness </span>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         </main>
